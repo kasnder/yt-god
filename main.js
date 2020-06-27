2ed49714697ece73b95932545e5bdf43354faf1a
Konrad Kollnig <konrad.kollnig@cs.ox.ac.uk>
2020-06-27 13:45:44 +0200

    Prettify code

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function monitorNavigated() {
     if (!items) {
         items = document.querySelector("#related #items");
         items.insertAdjacentHTML('beforebegin', '<div id="upnext" class="style-scope ytd-compact-autoplay-renderer" style="padding-bottom:12px;">Filtered Recommandations</div> <button id="updatebutton">Update </button>');
-        
+
         // button because it does not always work
         document.querySelector('#updatebutton').addEventListener("click", main);
     }
@@ -41,31 +41,30 @@ function main() {
     function onError(error) {
         console.log(`Error: ${error}`);
     }
-    
+
     function vhide(item) {
         if (item.hide) {
             // console.log("Hiding videos..", items.childNodes.length);
             var b = true; // not item hidden
-            for(s of items.childNodes) {
-                if(s.getElementsByClassName("ytd-thumbnail-overlay-resume-playback-renderer").length > 0) {
-                    s.style.display="none";
+            for (s of items.childNodes) {
+                if (s.getElementsByClassName("ytd-thumbnail-overlay-resume-playback-renderer").length > 0) {
+                    s.style.display = "none";
                     b = false;
                 }
             }
-            if(b) { setTimeOut(main, 5000);} // start again if no video is hidden
+            if (b) { setTimeOut(main, 5000); } // start again if no video is hidden
         }
     }
 
     function vsort() {
-
         toSort = Array.prototype.slice.call(items.childNodes, 0);
-        toSort.sort(function (a, b) {
-        return a.querySelector("#text.ytd-channel-name").innerText > b.querySelector("#text.ytd-channel-name").innerText;
-    });
-    items.innerHTML="";
-    for(var i = 0, l = toSort.length; i < l; i++) {
-        items.appendChild(toSort[i]);
-    }
+        toSort.sort(function(a, b) {
+            return a.querySelector("#text.ytd-channel-name").innerText > b.querySelector("#text.ytd-channel-name").innerText;
+        });
+        items.innerHTML = "";
+        for (var i = 0, l = toSort.length; i < l; i++) {
+            items.appendChild(toSort[i]);
+        }
     }
 
     browser.storage.sync.get("hide").then(vhide, onError);
